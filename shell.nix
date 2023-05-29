81306cbc20decf6905b37b7a413c0e0733756767
Masacaai <mohammed-shaik@outlook.com>
2023-05-28 21:50:26 -0500

    Added shell.nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+# shell.nix
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python = pkgs.python3;
+  python-with-my-packages = my-python.withPackages (p: with p; [
+    pygame
+    # other python packages you want
+  ]);
+in
+pkgs.mkShell {
+  packages = [
+    python-with-my-packages
+    # other dependencies
+  ];
+}
